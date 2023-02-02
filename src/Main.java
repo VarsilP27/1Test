0585ac23b76ba58419a3f677b09be32e0fd79d89
Varsil Patel <varsilpatel@Varsils-MacBook-Air.local>
2023-02-02 17:09:53 -0500

    question 2 complete

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        class Closet
+        {
+            private double getValue;
+            private String getClothesWithColour;
+            private String getNumOfClothesByDescription;
+        }
+        class Clothing
+        {
+            private String brand;
+            private String description;
+            private double price;
+            private String colour;
+        }
+    }
+
+
+
+}

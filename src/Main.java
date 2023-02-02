23f9e01bfd3dbad9fa338d8030dbf81fcb4e610b
varsilpatel <varsilpatel71@gmail.com>
2023-02-02 17:16:19 -0500

    question 3 complete

--- src/Main.java
+++ src/Main.java
@@ -15,9 +15,35 @@ public class Main {
             private String description;
             private double price;
             private String colour;
+
+                public Clothing(String brand, String description, double price, String colour) {
+                    this.setBrand(brand);
+                    this.setDescription(description);
+                    this.setPrice(price);
+                    this.setColour(colour);
+                }
+
+                public void setBrand(String brand) {
+                    this.brand = brand;
+                }
+
+                public void setDescription(String description) {
+                    this.description = description;
+                }
+
+                public void setPrice(double price) {
+                    this.price = price;
+                }
+
+                public void setColour(String colour) {
+                    this.colour = colour;
+                }
+            }
+
+
         }
     }
 
 
 
-}
+
